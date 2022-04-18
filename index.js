47743cc02efae0f33e585106039c92e5d876977f
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-19 00:57:04 +0300

    commit tree 24939e3f7f741cc59eee964d9e1ce92b4fa2e8c3

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+
+function findMatching(drivers,names)
+{
+    let match =[]
+    drivers.filter((name)=>{
+        if(name == names)
+        {
+            match.push(name)
+        }
+    }) 
+    return match
+
+}
+
+
+function fuzzyMatch(drivers,nameLetter)
+{
+    drivers.filter(name=>{
+        if(name[0]== nameLetter){
+            console.log(name);
+            return name
+        }
+    })
+}
+let matchName = (drivers,names)=>{
+    drivers.filter(driver=>{
+        if(driver.name === names)
+
+        {
+            console.log(driver);
+            return driver
+        }
+    })
+}
